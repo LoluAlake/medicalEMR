785ab24feb9995cf1ef5a8ec200d02b89c870008
charujamnis <charushila.jamnis@gmail.com>
2020-12-07 16:14:01 -0500

    Patient Visit screen added

--- src/com/dtcc/emr/service/PatientOperations.java
+++ src/com/dtcc/emr/service/PatientOperations.java
@@ -2,6 +2,7 @@ package com.dtcc.emr.service;
 
 import com.dtcc.emr.model.Address;
 import com.dtcc.emr.model.Patient;
+import com.dtcc.emr.model.PatientHistory;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
@@ -9,27 +10,28 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.time.ZoneId;
 
 
 public class PatientOperations {
     private PatientsInformation patientInformation;
-   // private int patientID;
+    // private int patientID;
     private String fullAddress;
     private String gender;
     //private static ResultSet rs;
     //TableColumn<Person, String> column1
-    public TableColumn<PatientsInformation,String> firstNameCol;
-    public TableColumn <PatientsInformation,String> lastNameCol;
-    public TableColumn <PatientsInformation,String>  emailCol;
-    public TableColumn <PatientsInformation,Integer> accountCol;
-    public TableColumn <PatientsInformation,Date>  dobCol;
-    public TableColumn <PatientsInformation,String> genderCol;
-    public TableColumn <PatientsInformation,String>  maritalStatusCol;
-    public TableColumn <PatientsInformation,String> phoneCol;
-    public TableColumn <PatientsInformation,String> addressCol;
+    public TableColumn<PatientsInformation, String> firstNameCol;
+    public TableColumn<PatientsInformation, String> lastNameCol;
+    public TableColumn<PatientsInformation, String> emailCol;
+    public TableColumn<PatientsInformation, Integer> accountCol;
+    public TableColumn<PatientsInformation, Date> dobCol;
+    public TableColumn<PatientsInformation, String> genderCol;
+    public TableColumn<PatientsInformation, String> maritalStatusCol;
+    public TableColumn<PatientsInformation, String> phoneCol;
+    public TableColumn<PatientsInformation, String> addressCol;
     //private TableView<Person> table = new TableView<Person>();
-    public TableView <PatientsInformation> table;
+    public TableView<PatientsInformation> table;
     Connection dbConnection;
 
     public PatientOperations() {
@@ -48,9 +50,9 @@ public class PatientOperations {
             stmt.setInt(1, patientInformation.getPatientId());
             stmt.executeUpdate();
 
-            String sqlAddress="update address set logicalDelete=1 where addressId=?";
-            PreparedStatement psAddress=con.prepareStatement(sqlAddress);
-            psAddress.setInt(1,patientInformation.getAddressId());
+            String sqlAddress = "update address set logicalDelete=1 where addressId=?";
+            PreparedStatement psAddress = con.prepareStatement(sqlAddress);
+            psAddress.setInt(1, patientInformation.getAddressId());
             psAddress.executeUpdate();
 
             con.close();
@@ -98,10 +100,10 @@ public class PatientOperations {
             psPatient.setString(4, patient.getFirst_name());
             psPatient.setString(5, patient.getLast_name());
 
-           // if(patient.getDob()==null){Date date=null; psPatient.setDate(6, date);}
+            // if(patient.getDob()==null){Date date=null; psPatient.setDate(6, date);}
             //else{
-                Date date = Date.valueOf(patient.getDob());
-                psPatient.setDate(6, date);
+            Date date = Date.valueOf(patient.getDob());
+            psPatient.setDate(6, date);
             //}
 
             psPatient.setString(7, String.valueOf(patient.getGender()));
@@ -145,28 +147,31 @@ public class PatientOperations {
         }
     }
 
-    public void updatePatient(Patient patient, Address address,int patientId, int addressId) throws SQLException {
+    public void updatePatient(Patient patient, Address address, int patientId, int addressId) throws SQLException {
 
         dbConnection = DatabaseConnection.getConnection();
-        String updateSql="update address set address1=?, address2=?,district=?,city=?,postalcode=?,country=? where addressId=?";
-        PreparedStatement addressPStatement=dbConnection.prepareStatement(updateSql);
-        addressPStatement.setString(1,address.getAddress1());
-        addressPStatement.setString(2,address.getAddress2());
-        addressPStatement.setString(3,address.getDistrict());
-        addressPStatement.setString(4,address.getCity());
-        addressPStatement.setString(5,address.getPostalcode());
-        addressPStatement.setString(6,address.getCountry());
-        addressPStatement.setInt(7,addressId);
+        String updateSql = "update address set address1=?, address2=?,district=?,city=?,postalcode=?,country=? where addressId=?";
+        PreparedStatement addressPStatement = dbConnection.prepareStatement(updateSql);
+        addressPStatement.setString(1, address.getAddress1());
+        addressPStatement.setString(2, address.getAddress2());
+        addressPStatement.setString(3, address.getDistrict());
+        addressPStatement.setString(4, address.getCity());
+        addressPStatement.setString(5, address.getPostalcode());
+        addressPStatement.setString(6, address.getCountry());
+        addressPStatement.setInt(7, addressId);
         addressPStatement.executeUpdate();
 
         String updatePatientSql = "update patient set accountnumber=?, first_name=?,last_name=?, dob=?, gender=?,height=?,weight=?,email=?,phonenumber=?,maritalstatus=?," +
                 "ssn=?,emergencyname=?,emergencycontact=?,employmentstatus=?,medicalhistory=?,allergies=?,medicines=? where patientId=?";
-        PreparedStatement psUpdatePatient=dbConnection.prepareStatement(updatePatientSql);
+        PreparedStatement psUpdatePatient = dbConnection.prepareStatement(updatePatientSql);
         psUpdatePatient.setInt(1, patient.getAccountnumber());
         psUpdatePatient.setString(2, patient.getFirst_name());
         psUpdatePatient.setString(3, patient.getLast_name());
-        if(patient.getDob()==null){Date date=null; psUpdatePatient.setDate(4, date);}
-        else{ Date date = Date.valueOf(patient.getDob());
+        if (patient.getDob() == null) {
+            Date date = null;
+            psUpdatePatient.setDate(4, date);
+        } else {
+            Date date = Date.valueOf(patient.getDob());
             psUpdatePatient.setDate(4, date);
         }
         psUpdatePatient.setString(5, String.valueOf(patient.getGender()));
@@ -198,7 +203,7 @@ public class PatientOperations {
             String gender = null;
 
             int patientId = rsList.getInt("patientId");
-            int addressId=rsList.getInt("addressId");
+            int addressId = rsList.getInt("addressId");
             String first_name = rsList.getString("first_name");
             String last_name = rsList.getString("last_name");
             String email = rsList.getString("email");
@@ -213,34 +218,33 @@ public class PatientOperations {
                 gender = "Male";
             }
 
-            Long ssn=rsList.getLong("ssn");
-            double height=rsList.getDouble("height");
-            double weight=rsList.getDouble("weight");
-            String address1=rsList.getString("address1");
-            String address2=rsList.getString("address2");
-            String city=rsList.getString("city");
-            String district=rsList.getString("district");
-            String country=rsList.getString("country");
-            String postalCode=rsList.getString("postalCode");
-            String emergencyname=rsList.getString("emergencyname");
-            String emergencycontact=rsList.getString("emergencycontact");
-            String medicalhistory=rsList.getString("medicalhistory");
-            String allergies=rsList.getString("allergies");
-            String medicines=rsList.getString("medicines");
-            String fullAddress = address1+", "+address2+", "+city+", "+district+", "+postalCode+", "+country;
-            String employmentstatus=rsList.getString("employmentstatus");
-            list.add(new PatientsInformation(patientId,addressId, first_name, last_name, email, accountnumber, dob, gender, phonenumber, maritalstatus, fullAddress,ssn,height,weight,address1,address2,city,district,country,postalCode,emergencyname,emergencycontact,medicalhistory,allergies,medicines,employmentstatus));
+            Long ssn = rsList.getLong("ssn");
+            double height = rsList.getDouble("height");
+            double weight = rsList.getDouble("weight");
+            String address1 = rsList.getString("address1");
+            String address2 = rsList.getString("address2");
+            String city = rsList.getString("city");
+            String district = rsList.getString("district");
+            String country = rsList.getString("country");
+            String postalCode = rsList.getString("postalCode");
+            String emergencyname = rsList.getString("emergencyname");
+            String emergencycontact = rsList.getString("emergencycontact");
+            String medicalhistory = rsList.getString("medicalhistory");
+            String allergies = rsList.getString("allergies");
+            String medicines = rsList.getString("medicines");
+            String fullAddress = address1 + ", " + address2 + ", " + city + ", " + district + ", " + postalCode + ", " + country;
+            String employmentstatus = rsList.getString("employmentstatus");
+            list.add(new PatientsInformation(patientId, addressId, first_name, last_name, email, accountnumber, dob, gender, phonenumber, maritalstatus, fullAddress, ssn, height, weight, address1, address2, city, district, country, postalCode, emergencyname, emergencycontact, medicalhistory, allergies, medicines, employmentstatus));
         }
 
         rsList.close();
         conObj.close();
-
         return list;
     }
 
     public TableView refreshPatientTable() throws SQLException {
-        ObservableList<PatientsInformation> data=getAllPatientsList();
-        table=new TableView<PatientsInformation>();
+        ObservableList<PatientsInformation> data = getAllPatientsList();
+        table = new TableView<PatientsInformation>();
 
         firstNameCol = new TableColumn<>("First Name");
         firstNameCol.setCellValueFactory(new PropertyValueFactory<>("first_name"));
@@ -269,11 +273,110 @@ public class PatientOperations {
         addressCol = new TableColumn<>("Address");
         addressCol.setCellValueFactory(new PropertyValueFactory<>("fullAddress"));
 
-        table.getColumns().addAll(firstNameCol,lastNameCol,emailCol,accountCol,dobCol,genderCol,maritalStatusCol,phoneCol,addressCol);
+        table.getColumns().addAll(firstNameCol, lastNameCol, emailCol, accountCol, dobCol, genderCol, maritalStatusCol, phoneCol, addressCol);
         table.setItems(data);
         return table;
 
     }
+
+    public static ObservableList<PatientHistoryInformation> getPatientHistory(int PatientID) {
+        final ObservableList<PatientHistoryInformation> data = FXCollections.observableArrayList();
+        Connection conn = DatabaseConnection.getConnection();
+        String patientHistoryStr = "select p.patientId, p.accountnumber,p.first_name,p.last_name,pro.procedureId, pro.cpt,pro.description,pro.cost,ph.purpose, ph.dateOfvisit, ph.nextappointment\n" +
+                "from patientHistory ph\n" +
+                "join patient p on ph.patientId=p.patientId\n" +
+                "join procedures pro on ph.procedureId=pro.procedureId\n" +
+                "where p.patientId like ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(patientHistoryStr);
+
+            if (PatientID == 0) {
+                ps.setString(1, "%" + "" + "%");
+            } else {
+                ps.setString(1, "%" + PatientID + "%");
+            }
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                LocalDate localDateVisit;
+                LocalDate localDateNextApp;
+                if (rs.getDate("dateOfVisit") == null) {
+                    localDateVisit = null;
+                } else {
+                    localDateVisit = rs.getDate("dateOfVisit").toLocalDate();
+                }
+
+                if (rs.getDate("nextappointment") == null) {
+                    localDateNextApp = null;
+                } else {
+                    localDateNextApp = rs.getDate("nextappointment").toLocalDate();
+                }
+
+                data.add(new PatientHistoryInformation(
+                        rs.getInt("patientId"),
+                        rs.getInt("procedureId"),
+                        rs.getInt("accountnumber"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getString("cpt"),
+                        rs.getString("description"),
+                        rs.getDouble("cost"),
+                        rs.getString("purpose"),
+                        localDateVisit,
+                        localDateNextApp
+                ));
+            }
+
+            conn.close();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+        return data;
+    }
+
+    public static ObservableList getAllPatientsNames() throws SQLException {
+
+        final ObservableList data = FXCollections.observableArrayList();
+        Connection conn = DatabaseConnection.getConnection();
+        String patientsName = "select patientId,first_name,last_name,accountnumber from patient where logicalDelete=0";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(patientsName);
+            while (rs.next()) {
+                String comboPatientData = rs.getString("first_name") + " " + rs.getString("last_name") + " - " + rs.getInt("accountnumber");
+                data.add(new PatientHistoryInformation(
+                        rs.getInt("patientId"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getInt("accountnumber")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public static ObservableList getAllCPTCodes() throws SQLException {
+
+        final ObservableList data = FXCollections.observableArrayList();
+        Connection conn = DatabaseConnection.getConnection();
+        String patientsName = "select cpt from procedures where logicalDelete=0";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(patientsName);
+            while (rs.next()) {
+                //String comboPatientData=rs.getString("first_name")+" "+ rs.getString("last_name")+" - "+ rs.getInt("accountnumber");
+                data.add(rs.getString("cpt"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+
 }
 
 
